d4c59918a03e817e8fc24551bbba4534179759f1
Aravinthan Chinnasamy <aravinthan.c@hippovideo.io>
2023-11-06 10:28:13 +0530

    minor changes made

--- billing-frontend/src/components/common/ButtonComponent.js
+++ billing-frontend/src/components/common/ButtonComponent.js
@@ -8,7 +8,7 @@ const ButtonComponent = ({
 
   return (
     <div className='inline-block'>
-      <button className={`font-bold min-w-[130px] text-[1.2em] px-[32px] py-[15px] m-2 scale-95 rounded-2xl hover:scale-100 duration-300 hover:ease-in ${buttonType}`} onClick={onClickCallback}>
+      <button className={`font-bold min-w-[130px] text-[1.2em] px-[32px] py-[15px] m-2 scale-90 rounded-2xl hover:scale-100 duration-300 hover:ease-in ${buttonType}`} onClick={onClickCallback}>
           {txt} 
       </button>
     </div>
