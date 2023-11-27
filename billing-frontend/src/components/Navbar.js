cc1c815373b467969d44141a0a14d43e22024ae2
SethuVenkatesh <sethuvenkateshvk@gmail.com>
2023-11-27 22:31:26 +0530

    navbar chnages

--- billing-frontend/src/components/Navbar.js
+++ billing-frontend/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import appLogo from '../assets/appLogo.png'
 
 export const Navbar = () => {
   return (
-    <div className='fixed top-0 w-full px-2 py-4 bg-blue-500'>
-      <div className='relative flex justify-between'>
-        <p className='text-white font-semibold'>Home</p>
-        <p className='text-white font-semibold'>Company</p>
-        <p className='text-white font-semibold'>Employee</p>
-        <p className='text-white font-semibold'>Billing</p>
-        <p className='text-white font-semibold'>Report</p>
+    <div className='fixed top-0 w-full  bg-[#f6f6e9] flex items-center justify-between h-[70px] shadow-md z-30'>
+      <div className='flex-1 h-full'>
+        <img src={appLogo} className='w-[150px] h-full'/>
+      </div>
+      <div className='flex-3 relative flex justify-between px-2 py-4                                                                                                                            '>
+        <p className=' font-semibold'>Home</p>
+        <p className=' font-semibold'>Company</p>
+        <p className=' font-semibold'>Employee</p>
+        <p className=' font-semibold'>Billing</p>
+        <p className=' font-semibold'>Report</p>
       </div>
       <p></p>
     </div>
